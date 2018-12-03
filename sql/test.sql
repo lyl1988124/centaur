3a9e050397d373bccfc52c4e6d42dcd6f344d4e2
lyl <lyl@didiglobal.com>
2018-12-03 18:05:55 +0800

    build file

---
+++ sql/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `address` int(11) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
